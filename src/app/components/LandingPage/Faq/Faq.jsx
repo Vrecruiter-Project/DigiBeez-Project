d12709cfc1d1329633f3fd5b22d02d1a575b62e3
Ayush Kumar <ayush.dhiman0610@gmail.com>
2025-02-04 16:07:29 +0530

    d

--- src/app/components/LandingPage/Faq/Faq.jsx
+++ src/app/components/LandingPage/Faq/Faq.jsx
@@ -13,40 +13,41 @@ import Grid from "@mui/material/Grid2";
 
 const faqData = [
   {
-    question: "What is the job Bookcamp program?",
-    answer: "Details about the job Bookcamp program.",
+    question: "What is the Search Engine Optimization (SEO)?",
+    answer: "This involves improving a website so it appears higher in search results, making it easier for people to find.",
   },
   {
-    question: "What is the refund policy for the program?",
-    answer: "Details about the refund policy.",
+    question: "What is the Pay-per-Click (PPC)",
+    answer: "This is a way to advertise where businesses pay each time someone clicks on their ad, usually shown on search engines.",
   },
   {
-    question: "Who is eligible for the program?",
-    answer: "Details about eligibility.",
+    question: "What is the Social Media Marketing",
+    answer: " This focuses on promoting products or services through platforms like Facebook, Instagram, and Twitter to engage with customers.",
   },
   {
-    question: "What is the duration of the program?",
-    answer: "Details about the program duration.",
+    question: "What is the Content Marketing?",
+    answer: " This is about creating valuable and relevant content, like blog posts or videos, to attract and inform customers.",
   },
   {
-    question: "How good is the placement record?",
-    answer: "Details about the program duration.",
+    question: "What is the Email Marketing?",
+    answer: " This includes sending newsletters or promotional messages directly to people's inboxes to keep them informed and encourage purchases.",
   }, {
-    question: "Which roles will I be eligible for?",
-    answer: "Details about the program duration.",
+    question: "What is the Affiliate Marketing?",
+    answer: "This involves partnering with others who promote a company’s products and earn a commission for every sale they help generate.",
   }, {
-    question: "How do I get my doubts resolved?",
-    answer: "Details about the program duration.",
-  }, {
-    question: "Where will the classes take place?",
-    answer: "Details about the program duration.",
-  }, {
-    question: "How many domain expert sessions and mock interviews will I have?",
-    answer: "Details about the program duration.",
-  }, {
-    question: "How will I benefit from industry mentors?",
-    answer: "Details about the program duration.",
-  },
+    question: "What is the  Influencer Marketing",
+    answer: "This strategy uses popular social media figures to promote a brand, tapping into their follower base for greater reach.",
+  }
+  //, {
+//     question: "Where will the classes take place?",
+//     answer: "Details about the program duration.",
+//   }, {
+//     question: "How many domain expert sessions and mock interviews will I have?",
+//     answer: "Details about the program duration.",
+//   }, {
+//     question: "How will I benefit from industry mentors?",
+//     answer: "Details about the program duration.",
+//   },
 ];
 
 const FAQ = ({ onButtonClick }) => {
